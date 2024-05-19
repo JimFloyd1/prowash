5bdc25a57dacb66ebab2fdde7ca55bbeda7c3dcc
JimFloyd1 <141870644+JimFloyd1@users.noreply.github.com>
2024-05-19 14:34:29 -0400

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <section className='py-24'>
+      <div className= 'container'>
+        <h1 className='text-3x1 font-bold'>Next TS Stater</h1>
+      </div>
+    </section>
+  );
+}
