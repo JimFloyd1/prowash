1e2f5cb774ff91c9c4b6a8076b188fb4b5cd7c34
Jim Floyd <carson_floyd2@yahoo.com>
2024-05-25 20:36:40 -0400

    updated , and ' to &#39 for lint rules

--- src/components/Benefits.tsx
+++ src/components/Benefits.tsx
@@ -26,8 +26,8 @@ const Benefits = () => {
     <div className="card-body">
         <h2 className="card-title">We Are Experts</h2>
         <p>
-    When you hire ProWash Pressure Washing, we come equipped with the latest gadgets and tools to do the job
-    When you rent a power washer, you have one project in mind. When you hire us we have the skills and tools to handle any unexpected developments</p>
+    When you hire ProWash Pressure Washing&#39 we come equipped with the latest gadgets and tools to do the job
+    When you rent a power washer&#39 you have one project in mind. When you hire us we have the skills and tools to handle any unexpected developments</p>
         <div className="card-actions justify-end">
         <button className="btn btn-primary">Contact Us For Help</button>
         </div>
@@ -45,7 +45,7 @@ const Benefits = () => {
     <div className="card-body">
         <h2 className="card-title">Increase the value of your property</h2>
         <p>
-    According to the National Association of Realtors, washing a house can add $10,000 to $15,000 to the sales price of some properties
+    According to the National Association of Realtors&#39 washing a house can add $10&#39000 to $15&#39000 to the sales price of some properties
     A good thorough professional pressure washing is an important aspect of attracting potential buyers and will help you sell faster​​ for more money</p>
         <div className="card-actions justify-end">
         </div>
@@ -66,7 +66,7 @@ const Benefits = () => {
         <h2 className="card-title">Improve your curb appeal</h2>
         <p>
     Curb appeal matters a lot and it impacts the potential resale of your home and property
-    When you have a professional clean your home, driveway and walkways you will instantly improve your curb appeal resulting in higher property value</p>
+    When you have a professional clean your home&#39 driveway and walkways you will instantly improve your curb appeal resulting in higher property value</p>
         <div className="card-actions justify-end">
         </div>
     </div>
@@ -109,7 +109,7 @@ const Benefits = () => {
     <div className="card-body">
         <h2 className="card-title">​No Work for You</h2>
         <p>
-    While you're relaxing with beach trips or barbecues leave the hard work to us. We are pros. This is what we do
+    While you&#39re relaxing with beach trips or barbecues leave the hard work to us. We are pros. This is what we do
     Homeowners Insurance does not cover you if your property is intentionally destroyed
         </p>
         <div className="card-actions justify-end">
@@ -130,7 +130,7 @@ const Benefits = () => {
     <div className="card-body">
         <h2 className="card-title">Safe for the environment​</h2>
         <p>
-    Our cleaning solutions are safe for plants, animals, and children
+    Our cleaning solutions are safe for plants&#39 animals&#39 and children
     We are mindful of the planet and minimize any environmental impact when doing the job
     We believe in keeping the planet green
         </p>
--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -109,36 +109,36 @@ return (
     className="rounded-lg shadow-2xl"
 />
     <div className = "pl-5">
-    <h1 className="text-5xl font-bold mb-5 tracking-tight text-center">Dirty Home? Don't Have Time?
+    <h1 className="text-5xl font-bold mb-5 tracking-tight text-center">Dirty Home? Don&#39t Have Time?
 We Would Love To Help You!</h1>
-    <p className="py-6">As a homeowner, it's important to keep the exterior of your home looking good and clean. This is not only important for curb appeal but it can protect your home from damage. A buildup of dirt, debris, mold & mildew can lead to a number of issues, including structural problems, if not dealt with right away.
-​The hot and humid weather of Myrtle Beach, SC can make a big impact on the appearance of your home. In addition to the usual accumulation of dirt, dust and grime that builds up on home siding, you also have to deal with the unsightly growth of algae, mold and mildew.
-​It also makes your home look nicer, which is important whether you are selling the house or plan to continue living there for some time
+    <p className="py-6">As a homeowner&#39 it&#39s important to keep the exterior of your home looking good and clean. This is not only important for curb appeal but it can protect your home from damage. A buildup of dirt&#39 debris&#39 mold & mildew can lead to a number of issues&#39 including structural problems&#39 if not dealt with right away.
+​The hot and humid weather of Myrtle Beach&#39 SC can make a big impact on the appearance of your home. In addition to the usual accumulation of dirt&#39 dust and grime that builds up on home siding&#39 you also have to deal with the unsightly growth of algae&#39 mold and mildew.
+​It also makes your home look nicer&#39 which is important whether you are selling the house or plan to continue living there for some time
 Our Gentle Wash House Washing is ideal for all types of home exteriors in the Myrtle Beach & Surrounding Areas.</p>
 
 <li className="flex space-x-2 rtl:space-x-reverse items-center pb-6">
             <svg className="flex-shrink-0 w-3.5 h-3.5 text-blue-600 dark:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
             <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
             </svg>
-            <span className="leading-tight">House Exterior Walls Pressure Washed with Gentle Pressure, Safe & Eco-Friendly Soap & Cleaning Agents, Not Harmful to Plants, Flowers or Children</span>
+            <span className="leading-tight">House Exterior Walls Pressure Washed with Gentle Pressure&#39 Safe & Eco-Friendly Soap & Cleaning Agents&#39 Not Harmful to Plants&#39 Flowers or Children</span>
         </li>
         <li className="flex space-x-2 rtl:space-x-reverse items-center pb-6">
             <svg className="flex-shrink-0 w-3.5 h-3.5 text-blue-600 dark:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
             <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
             </svg>
-            <span className="leading-tight">All House Trim, Windows, Soffits, Doors, Gutters, Garage Doors will be Pressure Washed</span>
+            <span className="leading-tight">All House Trim&#39 Windows&#39 Soffits&#39 Doors&#39 Gutters&#39&#39 Garage Doors will be Pressure Washed</span>
         </li>
         <li className="flex space-x-2 rtl:space-x-reverse items-center pb-6">
             <svg className="flex-shrink-0 w-3.5 h-3.5 text-blue-600 dark:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
             <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
             </svg>
-            <span className="leading-tight">All Porches, Driveways Walkways, and Entranceway areas will be pressure washed​</span>
+            <span className="leading-tight">All Porches&#39 Driveways Walkways&#39 and Entranceway areas will be pressure washed​</span>
         </li>
         <li className="flex space-x-2 rtl:space-x-reverse items-center pb-6">
             <svg className="flex-shrink-0 w-3.5 h-3.5 text-blue-600 dark:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
             <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
             </svg>
-            <span className="leading-tight pb-6">Patio & Deck Pressure Washing, Brick & Concrete Cleaning, Pavers & Tile Cleaning</span>
+            <span className="leading-tight pb-6">Patio & Deck Pressure Washing&#39 Brick & Concrete Cleaning&#39 Pavers & Tile Cleaning</span>
         </li>
         <button className="btn btn-primary">Learn More</button>
     </div>
@@ -189,13 +189,13 @@ We offer pressure washing solutions for a wide range of customers including:
             <svg className="flex-shrink-0 w-3.5 h-3.5 text-blue-600 dark:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
             <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
             </svg>
-            <span className="leading-tight">Prevent Damage: Regular pressure washing removes harmful substances like mold, mildew, and grime, extending the lifespan of your building's surfaces.</span>
+            <span className="leading-tight">Prevent Damage: Regular pressure washing removes harmful substances like mold&#39 mildew&#39 and grime&#39 extending the lifespan of your building&#39s surfaces.</span>
         </li>
         <li className="flex space-x-2 rtl:space-x-reverse items-center pb-6">
             <svg className="flex-shrink-0 w-3.5 h-3.5 text-blue-600 dark:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
             <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
             </svg>
-            <span className="leading-tight">Promote Health and Safety: Eliminate slippery surfaces, pollutants, and allergens, creating a safer and healthier environment for employees and customers.​</span>
+            <span className="leading-tight">Promote Health and Safety: Eliminate slippery surfaces&#39 pollutants&#39 and allergens&#39 creating a safer and healthier environment for employees and customers.​</span>
         </li>
         <li className="flex space-x-2 rtl:space-x-reverse items-center pb-6">
             <svg className="flex-shrink-0 w-3.5 h-3.5 text-blue-600 dark:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
@@ -228,24 +228,24 @@ We offer pressure washing solutions for a wide range of customers including:
     <div className = "pl-5">
     <h1 className="text-5xl font-bold mb-5 tracking-tight text-center">Pressure Washing for Property-Managers</h1>
     <p className="py-6">As a property manager or business owner you know the difficulties of keeping the exterior of your commercial building clean and in good condition. Throughout seasons properties can become dirty and worn. ProWash Pressure Washing can help maintain your building and offers several benefits to your business.</p>
-<p> Keeping a clean and tidy building is often correlated to the success of a business as it can attract new customers and increase property values. However, property managers and owners make the mistake of considering pressure washing services a luxury, whereas in reality, every business owner and manager needs the help of a pressure washing company. This is regardless of the type of property you manage. Here are a few reasons why:</p>
+<p> Keeping a clean and tidy building is often correlated to the success of a business as it can attract new customers and increase property values. However&#39 property managers and owners make the mistake of considering pressure washing services a luxury&#39 whereas in reality&#39 every business owner and manager needs the help of a pressure washing company. This is regardless of the type of property you manage. Here are a few reasons why:</p>
 <li className="flex space-x-2 rtl:space-x-reverse items-center pb-6 pt-8">
             <svg className="flex-shrink-0 w-3.5 h-3.5 text-blue-600 dark:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
             <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
             </svg>
-            <span className="leading-tight">Increased Property Value - A clean and appealing building is a valuable building. A professional and experienced pressure washing company can easily leave any building looking fresh and like new. This becomes very important for attracting new tenants to rent or buy property. Research by the National Association of Realtors says that pressure washing property will result in it selling faster and for $10,000 to $15,000 more than the original selling price.</span>
+            <span className="leading-tight">Increased Property Value - A clean and appealing building is a valuable building. A professional and experienced pressure washing company can easily leave any building looking fresh and like new. This becomes very important for attracting new tenants to rent or buy property. Research by the National Association of Realtors says that pressure washing property will result in it selling faster and for $10&#39000 to $15&#39000 more than the original selling price.</span>
         </li>
         <li className="flex space-x-2 rtl:space-x-reverse items-center pb-6">
             <svg className="flex-shrink-0 w-3.5 h-3.5 text-blue-600 dark:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
             <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
             </svg>
-            <span className="leading-tight">Cost Reduction - By keeping your building clean can prolong the lifespan of the material and avoid having to replace parts. Pressure washing is a form of preventive maintenance and it helps keep the exterior of your building clean and in good condition. By using gentle pressure with cleaning agents that are safe for plants and flowers will help you lower the maintenance cost of your buildings. ProWash Pressure Washing cleans exterior sidings, entryways, walkways, parking lots, and dumpster pads to maintain a fresh “like new” appearance and improving curb appeal.</span>
+            <span className="leading-tight">Cost Reduction - By keeping your building clean can prolong the lifespan of the material and avoid having to replace parts. Pressure washing is a form of preventive maintenance and it helps keep the exterior of your building clean and in good condition. By using gentle pressure with cleaning agents that are safe for plants and flowers will help you lower the maintenance cost of your buildings. ProWash Pressure Washing cleans exterior sidings&#39 entryways&#39 walkways&#39 parking lots&#39 and dumpster pads to maintain a fresh “like new” appearance and improving curb appeal.</span>
         </li>
         <li className="flex space-x-2 rtl:space-x-reverse items-center pb-6">
             <svg className="flex-shrink-0 w-3.5 h-3.5 text-blue-600 dark:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
             <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
             </svg>
-            <span className="leading-tight">Make A Good Impression - ​For managers or owners trying to rent out condos or apartment units, good impressions are critical to the success of your business. Qualified renters observe every part of a property when deciding where to live, this includes landscaping, and exterior building maintenance. An experienced pressure washing service can help property managers and owners ensure that every detail appears inviting and attractive.​</span>
+            <span className="leading-tight">Make A Good Impression - ​For managers or owners trying to rent out condos or apartment units&#39 good impressions are critical to the success of your business. Qualified renters observe every part of a property when deciding where to live&#39 this includes landscaping&#39 and exterior building maintenance. An experienced pressure washing service can help property managers and owners ensure that every detail appears inviting and attractive.​</span>
         </li>
         <li className="flex space-x-2 rtl:space-x-reverse items-center pb-6">
             <svg className="flex-shrink-0 w-3.5 h-3.5 text-blue-600 dark:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
@@ -270,7 +270,7 @@ We offer pressure washing solutions for a wide range of customers including:
     How Does Prowash of the GrandStrand Compare To Other Companies?
     </div>
     <div className="collapse-content">
-        <p>We have state of the art equipment and over 36 years of experience. We guarantee your satisfaction, 100%. Unlike some of the competition, every member of our team is fully insured and extensively trained.</p>
+        <p>We have state of the art equipment and over 36 years of experience. We guarantee your satisfaction&#39 100%. Unlike some of the competition&#39 every member of our team is fully insured and extensively trained.</p>
     </div>
     </div>
     <div className="collapse collapse-plus bg-base-200 ring-1 ring-current">
@@ -279,7 +279,7 @@ We offer pressure washing solutions for a wide range of customers including:
         What is your Area of Service?
     </div>
     <div className="collapse-content">
-        <p>We service Myrtle Beach, North Myrtle Beach, Carolina Forest, Murrells Inlet, Socastee, Garden City, Litchfield, Conway, and Surfside.</p>
+        <p>We service Myrtle Beach&#39 North Myrtle Beach&#39 Carolina Forest&#39 Murrells Inlet&#39 Socastee&#39 Garden City&#39 Litchfield&#39 Conway&#39 and Surfside.</p>
     </div>
     </div>
     <div className="collapse collapse-plus bg-base-200 ring-1 ring-current">
@@ -288,7 +288,7 @@ We offer pressure washing solutions for a wide range of customers including:
     Are Your Cleaning Methods And Products Environmentally Safe?
     </div>
     <div className="collapse-content">
-    Yes, all of our pressure washing and cleaning methods are environmentally safe. We strictly avoid using harsh chemicals that could damage your property, harm your pets or family, or negatively impact the local environment. We ensure that the chemicals and techniques we employ are 100% safe and effective for maintaining your property. Our cleaning methods are only aggressive towards stains, grime, dirt, mold, algae, and similar contaminants. We are committed to thoroughly and responsibly cleaning your exterior surfaces.    </div>
+    Yes&#39 all of our pressure washing and cleaning methods are environmentally safe. We strictly avoid using harsh chemicals that could damage your property&#39 harm your pets or family&#39 or negatively impact the local environment. We ensure that the chemicals and techniques we employ are 100% safe and effective for maintaining your property. Our cleaning methods are only aggressive towards stains&#39 grime&#39 dirt&#39 mold&#39 algae&#39 and similar contaminants. We are committed to thoroughly and responsibly cleaning your exterior surfaces.    </div>
     </div>
     <div className="collapse collapse-plus bg-base-200 ring-1 ring-current">
     <input type="radio" name="my-accordion-3" />
@@ -296,16 +296,16 @@ We offer pressure washing solutions for a wide range of customers including:
     How Frequently Is Pressure Washing Needed?
     </div>
     <div className="collapse-content">
-        <p>The frequency of pressure washing and soft washing your projects depends on the surface material. Most surfaces require cleaning at least once a year, but the condition of the surface and organic growth buildup also play into how often your home will need to be power washed. If you’re consistent with routine cleaning, organic growth won’t stand a chance and can be maintained all year long!</p>
+        <p>The frequency of pressure washing and soft washing your projects depends on the surface material. Most surfaces require cleaning at least once a year&#39 but the condition of the surface and organic growth buildup also play into how often your home will need to be power washed. If you’re consistent with routine cleaning&#39 organic growth won’t stand a chance and can be maintained all year long!</p>
     </div>
     </div>
     <div className="collapse collapse-plus bg-base-200 ring-1 ring-current">
     <input type="radio" name="my-accordion-3" />
     <div className="collapse-title text-xl font-medium">
-    Why Shouldn't I Pressure Wash My Home On My Own?
+    Why Shouldn&#39t I Pressure Wash My Home On My Own?
     </div>
     <div className="collapse-content">
-        <p>You can try, but we wouldn’t recommend it. Though it seems simple, many clients previously underestimated the knowledge and skill it takes to achieve a quality results. It also can take a lot of time. Our crew at ProWash Of The GrandStrand will come to your home with specialized equipment and extensive knowledge of safe procedures to ensure the best results possible.</p>
+        <p>You can try&#39 but we wouldn’t recommend it. Though it seems simple&#39 many clients previously underestimated the knowledge and skill it takes to achieve a quality results. It also can take a lot of time. Our crew at ProWash Of The GrandStrand will come to your home with specialized equipment and extensive knowledge of safe procedures to ensure the best results possible.</p>
     </div>
     </div>
 
