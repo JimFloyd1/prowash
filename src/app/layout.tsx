ab44ece48d923c27a26dd8f72159795ba6d3ed6f
Jim Floyd <carson_floyd2@yahoo.com>
2024-05-30 20:38:57 -0400

    x

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-import Navbar2 from "../components/Navbar2";
+// import Navbar2 from "../components/Navbar2";
 
 
 const inter = Inter({
