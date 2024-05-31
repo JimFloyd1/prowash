409f899d930130894baacfa492eef6c4d7341801
Jim Floyd <carson_floyd2@yahoo.com>
2024-05-31 14:18:35 -0400

    prop management section done

---
+++ src/app/services/(property-management)/hoa-trash-bin-pressure-washing/page.tsx
@@ -0,0 +1,224 @@
+import React from 'react'
+import Image from 'next/image'
+
+
+const page = () => {
+  return (
+<div>
+<div className="overflow-hidden py-24 sm:py-32">
+<div className="mx-auto max-w-7xl px-6 lg:px-8">
+<div className="flex items-center space-x-4">
+  <div className="flex-shrink-0 h-10 w-10 flex items-center justify-center rounded-lg bg-indigo-600 text-white">
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M9.813 15.904 9 18.75l-.813-2.846a4.5 4.5 0 0 0-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 0 0 3.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 0 0 3.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 0 0-3.09 3.09ZM18.259 8.715 18 9.75l-.259-1.035a3.375 3.375 0 0 0-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 0 0 2.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 0 0 2.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 0 0-2.456 2.456ZM16.894 20.567 16.5 21.75l-.394-1.183a2.25 2.25 0 0 0-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 0 0 1.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 0 0 1.423 1.423l1.183.394-1.183.394a2.25 2.25 0 0 0-1.423 1.423Z" />
+</svg>
+</div>
+<h1 className="mt-2 text-xl font-bold tracking-tight ">HOA Trash Bin Cleaning</h1>
+</div>
+<div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
+  <div className="lg:pr-8 lg:pt-4">
+    <div className="lg:max-w-lg">
+      <h2 className="mt-2 text-3xl font-bold tracking-tight sm:text-4xl">
+Trash Bin Cleaning for HOA's</h2>
+      <p className="mt-6 text-lg leading-8">A clean and healthy neighborhood is paramount. Make a positive impact on your community, and avoid unwanted smells and eyesores. An eco-friendly solution to clean, sanitize, and maintain your trash bins.</p>
+      <p className="mt-6 text-lg leading-8 mb-4">What do we do?</p>
+      <ul className="list-disc list-inside  mt-2 text-base leading-7">
+        <li>We wash, sanitize, and maintain trash bins so your residents don’t have to.</li>
+        <li>We deodorize the bins so they smell brand-new.</li>
+        <li>We help deter those pesky undesirables.</li>
+        <li>We kill 99.9% of germs, fungi, and viruses.</li>
+        <li>We work with you to make scheduling and maintenance as simple as possible.</li>
+        <li>And more!</li>
+      </ul>
+      <p className="mt-6 text-lg leading-8">Provide your residents’ with a healthier community. We understand the importance of keeping a neat and clean neighborhood and the significance this has on the health and safety of your residents. Dirty trash bins can pose potential health risks; not to mention the eyesore and terrible odors that come along with them. Maintain that clean curb appeal your strive for, while reducing unpleasant odors and preventing health risks that arise from bacteria, pests and mosquitoes.</p>
+    </div>
+  </div>
+  <Image
+    src="/prowash5.jpg"
+    height={500}
+    width={500}
+    className="rounded-3xl ring-4"
+    alt="loading.."
+  />
+</div>
+</div>
+</div>
+
+{/* Section Two */}
+<div className="bg-white rounded-t-3xl py-24 sm:py-32">
+  <div className="mx-auto max-w-7xl px-6 lg:px-8">
+    <div className="mx-auto max-w-2xl lg:text-center">
+      <h2 className="text-base font-semibold leading-7 text-indigo-600">SoftWash for Property Managers</h2>
+      <h3 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Professional Pressure Washing Services for Property Managers and Owners</h3>
+    </div>
+    <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
+      <dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
+        <div className="relative pl-16">
+          <dt className="text-base font-semibold leading-7 text-gray-900">
+            <div className="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-600">
+              <svg className="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" >
+                <path strokeLinecap="round" strokeLinejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+              </svg>
+            </div>
+
+            <h3>Ensure a clean and healthy neighborhood with trash bin cleaning</h3>
+          </dt>
+          <dd className="mt-2 text-base leading-7 text-gray-600">Trash Bin Cleaning for HOA's - A clean and healthy neighborhood is paramount. Regular trash bin cleaning helps make a positive impact on your community by avoiding unwanted smells and eyesores. Our eco-friendly solution ensures that trash bins are thoroughly cleaned, sanitized, and maintained, promoting a healthier living environment for all residents.</dd>
+        </div>
+        <div className="relative pl-16">
+          <dt className="text-base font-semibold leading-7 text-gray-900">
+            <div className="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-600">
+              <svg className="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" >
+                <path strokeLinecap="round" strokeLinejoin="round" d="M9.813 15.904 9 18.75l-.813-2.846a4.5 4.5 0 0 0-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 0 0 3.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 0 0 3.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 0 0-3.09 3.09ZM18.259 8.715 18 9.75l-.259-1.035a3.375 3.375 0 0 0-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 0 0 2.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 0 0 2.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 0 0-2.456 2.456ZM16.894 20.567 16.5 21.75l-.394-1.183a2.25 2.25 0 0 0-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 0 0 1.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 0 0 1.423 1.423l1.183.394-1.183.394a2.25 2.25 0 0 0-1.423 1.423Z" />
+              </svg>
+            </div>
+
+            <h3>Comprehensive trash bin cleaning and sanitization services</h3>
+          </dt>
+          <dd className="mt-2 text-base leading-7 text-gray-600">What Do We Do? - At ProWash Pressure Washing, we wash, sanitize, and maintain trash bins so your residents don’t have to. Using high-pressure hot water washes, we leave bins squeaky clean and deodorized, making them smell brand-new. This comprehensive cleaning process kills 99.9% of germs, fungi, and viruses, helping to maintain a hygienic environment.</dd>
+        </div>
+        <div className="relative pl-16">
+          <dt className="text-base font-semibold leading-7 text-gray-900">
+            <div className="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-600">
+            <svg className="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" >
+              <path strokeLinecap="round" strokeLinejoin="round" d="M6.633 10.25c.806 0 1.533-.446 2.031-1.08a9.041 9.041 0 0 1 2.861-2.4c.723-.384 1.35-.956 1.653-1.715a4.498 4.498 0 0 0 .322-1.672V2.75a.75.75 0 0 1 .75-.75 2.25 2.25 0 0 1 2.25 2.25c0 1.152-.26 2.243-.723 3.218-.266.558.107 1.282.725 1.282m0 0h3.126c1.026 0 1.945.694 2.054 1.715.045.422.068.85.068 1.285a11.95 11.95 0 0 1-2.649 7.521c-.388.482-.987.729-1.605.729H13.48c-.483 0-.964-.078-1.423-.23l-3.114-1.04a4.501 4.501 0 0 0-1.423-.23H5.904m10.598-9.75H14.25M5.904 18.5c.083.205.173.405.27.602.197.4-.078.898-.523.898h-.908c-.889 0-1.713-.518-1.972-1.368a12 12 0 0 1-.521-3.507c0-1.553.295-3.036.831-4.398C3.387 9.953 4.167 9.5 5 9.5h1.053c.472 0 .745.556.5.96a8.958 8.958 0 0 0-1.302 4.665c0 1.194.232 2.333.654 3.375Z" />
+            </svg>
+            </div>
+
+            <h3>Simplified scheduling and maintenance for convenience</h3>
+          </dt>
+          <dd className="mt-2 text-base leading-7 text-gray-600">Convenient Scheduling and Maintenance - We work with you to make scheduling and maintenance as simple as possible. Our flexible approach ensures that trash bin cleaning fits seamlessly into your community's routine, minimizing disruption while maximizing the benefits of regular cleaning. This convenience makes it easy for HOA's to keep their neighborhoods clean and odor-free.​</dd>
+        </div>
+        <div className="relative pl-16">
+          <dt className="text-base font-semibold leading-7 text-gray-900">
+            <div className="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-600">
+              <svg className="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" >
+                <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 18.75a60.07 60.07 0 0 1 15.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 0 1 3 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 0 0-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 0 1-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 0 0 3 15h-.75M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm3 0h.008v.008H18V10.5Zm-12 0h.008v.008H6V10.5Z" />
+              </svg>
+            </div>
+
+          <h3>Enhance curb appeal and reduce health risks</h3>
+          </dt>
+          <dd className="mt-2 text-base leading-7 text-gray-600">Maintain Curb Appeal and Reduce Health Risks - Providing residents with a cleaner community enhances curb appeal and reduces unpleasant odors. Regular cleaning and sanitizing of trash bins prevent health risks associated with bacteria, pests, and mosquitoes. By maintaining clean trash bins, ProWash Pressure Washing helps create a safer and more attractive neighborhood for all residents. </dd>
+        </div>
+      </dl>
+    </div>
+  </div>
+</div>
+
+
+{/* FORM */}
+<form>
+<div className="overflow-hidden ">
+        <div className="mx-auto max-w-7xl px-6 lg:px-8 ">
+          <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
+            <div className="lg:text-center">
+              <h2 className="mt-2 text-3xl font-bold tracking-tight sm:text-4xl ">Hire a Professional Pressure Washer</h2>
+              <h3 className="text-xl font-semibold leading-7 ">Contact us Today!</h3>
+            </div>
+            <div className="mt-10 flex flex-col lg:flex-row items-center gap-10">
+              <div className="lg:w-2/3 text-lg">
+                <div className = "flex items-center space-x-4">
+                <div className="flex-shrink-0 h-10 w-10 flex items-center justify-center rounded-lg bg-indigo-600">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className=" text-white size-6">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z" />
+                </svg>
+                </div>
+                <p className="leading-8 ">Ready to take the next step? Contact us today for a fast quote. Our team of experts is ready to help you keep your home looking its best.</p>
+                </div>
+                </div>
+              <div className="lg:w-1/3 w-full p-6 bg-gradient-to-r from-indigo-200 to-indigo-400 rounded-xl shadow-lg mb-9 ring-2 ring-black">
+                <div className="grid grid-cols-1 gap-y-6">
+                  <div>
+                    <label htmlFor="name" className="block text-smd font-medium text-black">
+                      Name
+                    </label>
+                    <input
+                      type="text"
+                      name="name"
+                      id="name"
+                      autoComplete="name"
+                      className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                      required
+                    />
+                  </div>
+
+                  <div>
+                    <label htmlFor="phone" className="block text-smd font-medium text-black">
+                      Phone
+                    </label>
+                    <input
+                      type="tel"
+                      name="phone"
+                      id="phone"
+                      autoComplete="phone"
+                      className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                      required
+                    />
+                  </div>
+
+                  <div>
+                    <label htmlFor="email" className="block text-md font-medium text-black">
+                      Email
+                    </label>
+                    <input
+                      type="email"
+                      name="email"
+                      id="email"
+                      autoComplete="email"
+                      className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                      required
+                    />
+                  </div>
+
+                  <div>
+                    <label htmlFor="address" className="block text-md font-medium text-black">
+                      Address
+                    </label>
+                    <input
+                      type="address"
+                      name="address"
+                      id="address"
+                      autoComplete="address"
+                      className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                      required
+                    />
+                  </div>
+
+                  <div>
+                    <label htmlFor="message" className="block text-md font-medium text-black">
+                      Message (optional)
+                    </label>
+                    <textarea
+                      name="message"
+                      id="message"
+                      rows={4}
+                      className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+
+                    ></textarea>
+                  </div>
+                  <div>
+                    <button
+                      type="submit"
+                      className="w-full inline-flex justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                    >
+                      Submit For A Fast Quote
+                    </button>
+                  </div>
+                </div>
+              </div>
+        </div>
+        </div>
+        </div>
+        </div>
+    </form>
+
+
+
+
+
+    </div>
+  )
+}
+
+export default page
